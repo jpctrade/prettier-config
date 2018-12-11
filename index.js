0bde18b5820995a3c2ccfe9c9b3ae2f249c3af77
nakazavva <nakazavva@gmail.com>
2018-12-11 13:30:13 +0900

    tabWidth=2 for json

--- index.js
+++ index.js
@@ -5,4 +5,12 @@ module.exports = {
     singleQuote: true,
     tabWidth: 4,
     trailingComma: 'all',
+    overrides: [
+        {
+            files: '*.json',
+            options: {
+                tabWidth: 2,
+            },
+        },
+    ],
 };
